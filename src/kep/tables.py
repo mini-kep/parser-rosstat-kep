facd97311b667dd1bf9e79350a3b8e2c41a2cd71
Sergey <varnie29a@mail.ru>
2017-07-06 23:05:02 +0500

    fixed: NameError: name 'tables' is not defined

--- src/kep/tables.py
+++ src/kep/tables.py
@@ -409,7 +409,7 @@ class Header:
 # regression tests - after bug fixes on occasional errors
 def test_csv_has_no_null_byte():
     csv_path = files.get_path_csv(2015, 2)
-    z = csv_path.read_text(encoding=tables.ENC)
+    z = csv_path.read_text(encoding=ENC)
     assert "\0" not in z
     
 if __name__ == "__main__":
