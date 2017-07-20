74b127d8540bff8a14c3c32e8906cb843969460c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-07-20 17:15:28 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -11,8 +11,8 @@ PROJECT_DIR = Path(__file__).parent
 def walk_files(directory: Path):
     for _insider in directory.iterdir():
         if _insider.is_dir():
-            for _sub in walk_files(_insider.resolve(
-            )):  # When is folder we recall this function to iterate insider elements
+            subs =  walk_files(_insider.resolve())
+            for _sub in subs:  
                 yield _sub.resolve()
         else:
             yield _insider.resolve()
@@ -30,7 +30,7 @@ def pep8(ctx, folder="kep"):
     files = filter(lambda x: x.suffix == ".py", walk_files(path))
     for f in files:
         print("Formatting", f)
-        # FIXME: may
+        # FIXME: may use 'import autopep8' without console
         ctx.run("autopep8 --aggressive --aggressive --in-place {}".format(f))
 
 
@@ -41,17 +41,18 @@ def clean(ctx):
         print("Removing", f)
         f.unlink()
 
-# WONTFIX: add flake8
-# Lint using flake8
-# lint:
-#	flake8 --exclude=lib/,bin/,docs/conf.py .
 
-#@task
-# def lint():
-#    """lint - check style with flake8."""
-#    run('flake8 {{ cookiecutter.repo_name|replace('-', '_') }} tests')
+@task
+def lint(ctx, folder="src/kep"):
+    """Check style with flake8
+    
+       See more flake8 usage at:
+           https://habrahabr.ru/company/dataart/blog/318776/
+    """
+    # E501 line too long
+    # --max-line-length=100           
+    ctx.run('flake8 {} --exclude test* --ignore E501'.format())
 
-# https://habrahabr.ru/company/dataart/blog/318776/
 
 
 @task
@@ -69,8 +70,8 @@ def ls(ctx):
     """List directory"""
     cmd = "dir /b"
     result = ctx.run(cmd, hide=False, warn=True)
-    # print(result.ok)
-    # print(result.stdout.splitlines())
+    print(result.ok)
+    print(result.stdout.splitlines())
 
 
 ns = Collection()
@@ -86,82 +87,37 @@ if platform == 'win32':
     ns.configure({'run': {'shell': environ['COMSPEC']}})
 
 
-"""
 #################################################################################
 # GLOBALS                                                                       #
 #################################################################################
 
-PROJECT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
-BUCKET = {{ cookiecutter.s3_bucket }}
-PROFILE = {{ cookiecutter.aws_profile }}
-PROJECT_NAME = {{ cookiecutter.repo_name }}
-PYTHON_INTERPRETER = {{ cookiecutter.python_interpreter }}
-
-ifeq (,$(shell which conda))
-HAS_CONDA=False
-else
-HAS_CONDA=True
-endif
+#PROJECT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+#BUCKET = {{ cookiecutter.s3_bucket }}
+#PROFILE = {{ cookiecutter.aws_profile }}
+#PROJECT_NAME = {{ cookiecutter.repo_name }}
+#PYTHON_INTERPRETER = {{ cookiecutter.python_interpreter }}
 
 #################################################################################
 # COMMANDS                                                                      #
 #################################################################################
 
-## Install Python Dependencies
-requirements: test_environment
-	pip install -r requirements.txt
-
 ## Make Dataset
-data: requirements
-	$(PYTHON_INTERPRETER) src/data/make_dataset.py
+#data: requirements
+#	$(PYTHON_INTERPRETER) src/data/make_dataset.py
 
-## Delete all compiled Python files
-clean:
-	find . -name "*.pyc" -exec rm {} \;
-
-## Lint using flake8
-lint:
-	flake8 --exclude=lib/,bin/,docs/conf.py .
 
 ## Upload Data to S3
-sync_data_to_s3:
-ifeq (default,$(PROFILE))
-	aws s3 sync data/ s3://$(BUCKET)/data/
-else
-	aws s3 sync data/ s3://$(BUCKET)/data/ --profile $(PROFILE)
-endif
+#sync_data_to_s3:
+#ifeq (default,$(PROFILE))
+#	aws s3 sync data/ s3://$(BUCKET)/data/
+#else
+#	aws s3 sync data/ s3://$(BUCKET)/data/ --profile $(PROFILE)
+#endif
 
-## Download Data from S3
-sync_data_from_s3:
-ifeq (default,$(PROFILE))
-	aws s3 sync s3://$(BUCKET)/data/ data/
-else
-	aws s3 sync s3://$(BUCKET)/data/ data/ --profile $(PROFILE)
-endif
-
-## Set up python interpreter environment
-create_environment:
-ifeq (True,$(HAS_CONDA))
-		@echo ">>> Detected conda, creating conda environment."
-ifeq (3,$(findstring 3,$(PYTHON_INTERPRETER)))
-	conda create --name $(PROJECT_NAME) python=3
-else
-	conda create --name $(PROJECT_NAME) python=2.7
-endif
-		@echo ">>> New conda env created. Activate with:\nsource activate $(PROJECT_NAME)"
-else
-	@pip install -q virtualenv virtualenvwrapper
-	@echo ">>> Installing virtualenvwrapper if not already intalled.\nMake sure the following lines are in shell startup file\n\
-	export WORKON_HOME=$$HOME/.virtualenvs\nexport PROJECT_HOME=$$HOME/Devel\nsource /usr/local/bin/virtualenvwrapper.sh\n"
-	@bash -c "source `which virtualenvwrapper.sh`;mkvirtualenv $(PROJECT_NAME) --python=$(PYTHON_INTERPRETER)"
-	@echo ">>> New virtualenv created. Activate with:\nworkon $(PROJECT_NAME)"
-endif
-
-## Test python environment is setup correctly
-test_environment:
-	$(PYTHON_INTERPRETER) test_environment.py
-
-#################################################################################
-# PROJECT RULES                                                                 #
-#################################################################################
-"""
+### Download Data from S3
+#sync_data_from_s3:
+#ifeq (default,$(PROFILE))
+#	aws s3 sync s3://$(BUCKET)/data/ data/
+#else
+#	aws s3 sync s3://$(BUCKET)/data/ data/ --profile $(PROFILE)
+#endif
