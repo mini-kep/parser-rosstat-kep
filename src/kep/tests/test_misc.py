c7557ca170b2adb1b097a8eb9014c3363e533686
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-07-28 10:38:30 +0300

    Update test_misc.py

--- src/kep/tests/test_misc.py
+++ src/kep/tests/test_misc.py
@@ -5,7 +5,7 @@ from .. import files
 
 
 def test_csv_has_no_null_byte():
-    csv_path = files.get_path_csv(2015, 2)
+    csv_path = files.locate_csv(2015, 2)
     z = csv_path.read_text(encoding=files.ENC)
     assert "\0" not in z
 
