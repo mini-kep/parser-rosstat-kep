a997a568be234126bb48006bc153780c3d5a062a
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-08-07 11:53:16 +0300

    #51 - empty funcs with TODOs for S3 bucket operations

--- todo_task_boto_s3_sync.py
+++ todo_task_boto_s3_sync.py
@@ -32,3 +32,42 @@ except botocore.exceptions.ClientError as e:
         print("The object does not exist.")
     else:
         raise
+        
+        
+class RawData:
+
+    def init(year, month):
+        self.date = year, month
+        # NOT NOW: may check if date is valid 
+        
+        #S3 bucket:
+        #'mini-kep-raw-data'
+        
+        
+    def download_from_s3(self):
+        # TODO 1: *download_raw_data_from_s3* download data/raw from s3
+        pass    
+              
+        # listing all objects in S3 bucket https://goo.gl/p4SCv4
+
+    def upload_to_s3(self):
+        # TODO 2: *upload_raw_data_to_s3* data/raw to s3
+        pass    
+
+
+def upload_html_docsto_s3():
+    pass
+    # TODO 3: *doc_upload* upload docs to from s3 mini-kep-docs
+    # upload all files from doc\_build\html\ 
+    # to aws https://mini-kep-docs.s3.amazonaws.com/
+    # mini-kep-docs + is region neded?
+        
+
+# for reference:     
+# Download Data from S3
+# sync_data_from_s3:
+# ifeq (default,$(PROFILE))
+#	aws s3 sync s3://$(BUCKET)/data/ data/
+# else
+#	aws s3 sync s3://$(BUCKET)/data/ data/ --profile $(PROFILE)
+# endif
