db2f50684019910e0a4668cdd48ad36ad4ea1808
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-08-26 02:33:26 +0300

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,8 +1,41 @@
-# -*- coding: utf-8 -*-
+"""
++----------------------------------------------+--------------------------+
+| Step                                         | Actor                    |
++==============================================+==========================+
+| 1. Open csv file as connection               | reader.open_csv()        |
++----------------------------------------------+--------------------------+
+| 2. Convert file to list of Rows instances    |                          |
++----------------------------------------------+                          |
+| 3. Split list to segments and                |                          |
+|    get parsing definition for each segment   | reader.Reader().items()  |
++----------------------------------------------+                          |
+| 4. Supply segment and a parsing definition   |                          |
+|    for further processing                    |                          |
++----------------------------------------------+--------------------------+
+| 5. Group rows to tables                      |                          |
+|                                              | parcer.extract_tables()  |
++----------------------------------------------+                          |
+| 6. Extract variable name and unit of         |                          |
+|    measurement from each table               |                          |
++----------------------------------------------+                          |
+| 7. Filter defined tables and check all       |                          |
+|    required variables were read              |                          |
++----------------------------------------------+--------------------------+
+| 8. Submit tables to an emitter instance      |                          |
++----------------------------------------------+                          |
+| 9. Import datapoints from table datarows     | emitter.Emitter()        |
+|    by frequency                              |                          |
++----------------------------------------------+                          |
+| 10. Create dataframes by frequency           |                          |
++----------------------------------------------+--------------------------+
+| 11. Validate data in dataframes              | validator.Validator      |
++----------------------------------------------+--------------------------+
+| 12. Save dataframes in csv files             | Vintage().save()         |
++----------------------------------------------+--------------------------+
+"""
 import pandas as pd
 import io
 
-# from runner.py
 from csv2df.specification import Definition, Specification
 from csv2df.reader import Reader, open_csv
 from csv2df.parcer import extract_tables
