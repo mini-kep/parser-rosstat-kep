ed35f34ec4cdbe65b79d9602d907aea842e4ac89
jurgis <jurgis.pralgauskis@gmail.com>
2017-08-28 11:23:18 +0300

    some polish + apidoc generated rsts

---
+++ doc/csv2df.validator.rst
@@ -0,0 +1,7 @@
+csv2df.validator module
+=======================
+
+.. automodule:: csv2df.validator
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- src/csv2df/__init__.py
+++ src/csv2df/__init__.py
@@ -1,9 +1,2 @@
 """parses and converts fuzzy csv to pandsa data frames"""
-__all__ = """helpers
-specification
-reader
-parcer
-runner
-validator
-util_label
-util_row_splitter""".split()
+__all__ = ['helpers', 'specification', 'reader', 'parcer', 'runner', 'validator', 'util_label', 'util_row_splitter']
