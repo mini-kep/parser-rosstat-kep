56cf2aa4a1c5868d75a30795d9eaf9c83208d2f9
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-08-30 16:04:32 +0300

    changed access example

--- src/example_access_data.py
+++ src/example_access_data.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Suggested code to read pandas dataframes from stable URL.
-
-To add:
-- caching to local file
-
-"""
+"""Suggested code to read pandas dataframes from stable URL."""
 
 import pandas as pd
 
@@ -13,13 +8,16 @@ def get_dataframe(freq):
     url_base = "https://raw.githubusercontent.com/epogrebnyak/mini-kep/master/data/processed/latest/{}"
     filename = "df{}.csv".format(freq)
     url = url_base.format(filename)
-    # canonical wrapper for pd.read_csv
-    return pd.read_csv(url), 
-                       #converters={'time_index': pd.to_datetime},
-                       #index_col='time_index')
+    # wrapper for pd.read_csv    
+    return pd.read_csv(url, 
+                       converters={'time_index': pd.to_datetime},
+                       index_col='time_index')
 
-if '__main__' == __name__:
+# TODO:
+# - caching to local file    
+
     
+if '__main__' == __name__:    
     dfa = get_dataframe('a')
     dfq = get_dataframe('q')
     dfm = get_dataframe('m')
