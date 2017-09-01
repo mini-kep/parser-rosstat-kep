c72f53f1c073f4c9e01720367c23df1bcb2e78ba
mousehead <mousehead@github.com>
2017-09-01 13:14:25 +0300

    Visualizer generates indicators (#78.2)

--- src/frontpage/images/make_images.py
+++ src/frontpage/images/make_images.py
@@ -1,40 +1,69 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 from pathlib import Path
 
 def get_root():
-	return Path(__file__).parents[3]
+    return Path(__file__).parents[3]
 
 import access_dfs
 
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
 class Visualizer:
-	def __init__(self):
-		self.dfs = access_dfs.get_dfs_from_web()
-		self.rootfolder = get_root()
-		self.pngfolder = self.rootfolder / 'output' / 'png'
+    def __init__(self):
+        self.dfs = access_dfs.get_dfs_from_web()
+        self.rootfolder = get_root()
+        self.pngfolder = self.rootfolder / 'output' / 'png'
 
-	def gen_splines(self):
-		outpath = self.pngfolder / 'splines'
+    def gen_splines(self, freq='m'):
+        outpath = self.pngfolder / 'splines'
 
-		cols = self.dfs['m'].columns
-		cols = cols.drop(['year', 'month'])   
+        cols = self.dfs[freq].columns
+        cols = cols.drop(['year', 'month'])   
 
-		fig = plt.figure(figsize=(2, 0.6))	
-		for col in cols:
-			ts = self.dfs['m'][col]
+        plt.style.use('ggplot')
+        fig = plt.figure(figsize=(2, 0.6))    
 
-			axes = fig.add_subplot(1, 1, 1, facecolor='white')
-			axes.plot(ts)
-			plt.axis('off')
+        for col in cols:
+            ts = self.dfs[freq][col]
 
-			name = "{}.png".format(col)
-			plt.savefig(str(outpath / name))
-			
-			plt.cla()  # close axes
+            axes = fig.add_subplot(1, 1, 1, facecolor='white')
+            axes.plot(ts)
+            plt.axis('off')
+
+            name = "{}.png".format(col)
+            plt.savefig(str(outpath / name))
+            
+            plt.cla()  # close axes
+
+    def gen_ts(self, fig, ts, timerange, fpath):
+        axes = fig.add_subplot(1, 1, 1, facecolor='white')
+        axes.plot(ts)
+        axes.set_xlim()
+        fig.autofmt_xdate()
+        plt.savefig(fpath)
+        plt.cla()
+
+    def gen_indicators(self, freq='m'):
+        outpath = self.pngfolder / 'indicators'
+
+        cols = self.dfs[freq].columns
+        cols = cols.drop(['year', 'month'])   
+
+        plt.style.use('bmh')
+        fig = plt.figure(figsize=(5,5))
+
+        for col in cols:
+            ts = self.dfs[freq][col]
+            fpath = str(outpath / "{}.png".format(col))
+
+            self.gen_ts(fig, ts, 
+                        [datetime.date(1998, 12, 31), datetime.date(2017, 12, 31)],
+                        fpath)
+        
 
 
 viz = Visualizer()
-viz.gen_splines()
+viz.gen_splines('m')
+viz.gen_indicators('m')
--- src/frontpage/images/test_access_dfs.py
+++ src/frontpage/images/test_access_dfs.py
@@ -9,44 +9,44 @@ import access_dfs as a_d
 
 def do_test():
 
-	print('\nValidating compose_url(): ', end='')
-	try:
-		assert a_d.compose_url('x') == (""
-			"https://raw.githubusercontent.com/"
-			"epogrebnyak/mini-kep/master/data/processed/latest/dfx.csv")
-		print('OK')
-	except AssertionError:
-		print('FAILED!')
-		return
+    print('\nValidating compose_url(): ', end='')
+    try:
+        assert a_d.compose_url('x') == (""
+            "https://raw.githubusercontent.com/"
+            "epogrebnyak/mini-kep/master/data/processed/latest/dfx.csv")
+        print('OK')
+    except AssertionError:
+        print('FAILED!')
+        return
 
-	print('Trying to load dataframe: ', end='')
-	try:
-		response = request.urlopen(a_d.compose_url('a'))
-		assert response.code == 200
-		print('OK')
-	except AssertionError:
-		print('FAILED! (response code != 200)')
-		return
-	except Exception:
-		print('FAILED! (unknown error)')
-		return
+    print('Trying to load dataframe: ', end='')
+    try:
+        response = request.urlopen(a_d.compose_url('a'))
+        assert response.code == 200
+        print('OK')
+    except AssertionError:
+        print('FAILED! (response code != 200)')
+        return
+    except Exception:
+        print('FAILED! (unknown error)')
+        return
 
-	dfs = None
-	print('Executing get_dfs_from_web(): ', end='')
-	try:
-		dfs = a_d.get_dfs_from_web()
-		print('OK')
-	except Exception:
-		print('FAILED! (failed to load)')
-		return
+    dfs = None
+    print('Executing get_dfs_from_web(): ', end='')
+    try:
+        dfs = a_d.get_dfs_from_web()
+        print('OK')
+    except Exception:
+        print('FAILED! (failed to load)')
+        return
 
-	try:
-		assert dfs['a'].shape == (18, 31)
-		assert dfs['q'].shape == (74, 38)
-		assert dfs['m'].shape == (223, 36)
-	except AssertionError:
-		print('WARNING: Dataframes size changed',
-				'or they are parsed incorrectly')
+    try:
+        assert dfs['a'].shape == (18, 31)
+        assert dfs['q'].shape == (74, 38)
+        assert dfs['m'].shape == (223, 36)
+    except AssertionError:
+        print('WARNING: Dataframes size changed',
+                'or they are parsed incorrectly')
 
 
 do_test()
