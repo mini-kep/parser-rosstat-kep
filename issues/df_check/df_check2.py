814ffd427e81ae416acbc316810edbaee0644ebb
zarak <zarak.mahmud@utoronto.ca>
2017-09-02 01:42:46 +0500

    Use aggregation functions by direction

--- issues/df_check/df_check2.py
+++ issues/df_check/df_check2.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import pandas as pd
-from io import StringIO
-
 
 # TODO <https://github.com/epogrebnyak/mini-kep/issues/61>
 
@@ -10,12 +7,15 @@ from io import StringIO
 #  1. absolute values by month/qtr accumulate to qtr/year (with some delta for rounding)
 #  2. rog rates accumulate to yoy (with some delta for rounding)
 #
-########################################################################################
+##############################################################################
+
+
+ANNUAL_FREQUENCY = 'A'
 
 
 def get_levels(columns):
     """Find the columns that are levels.
-    
+
     Args:
         columns: List of column names from dataframe.
 
@@ -29,51 +29,72 @@ def get_levels(columns):
 
 def get_rates(columns):
     """Find the columns that are rates.
-    
+
     Args:
         columns: List of column names from dataframe.
 
     Returns:
         Filtered list of rates.
     """
-    return [column for column in columns 
+    return [column for column in columns
             if '_rog' in column and 'GOV' not in column]
 
 
 def aggregate_levels(df, freq='A'):
+    df = levels_df(df)
     return df.resample(freq).sum()
 
 
 def aggregate_rates(df, freq='A'):
+    df = rates_df(df)
     df = (df / 100).cumprod()  # Compute annualized values
     z = df.resample(freq).sum()
-    z = z / z.shift() * 100
-    return z
+    return z / z.shift() * 100
 
 
-def build_levels_df(df):
+def levels_df(df):
     levels = get_levels(df.columns)
     return df[levels]
 
 
-def build_rates_df(df):
+def rates_df(df):
     rates = get_rates(df.columns)
     return df[rates]
 
 
+def build(df):
+    return levels_df(df).join(rates_df(df))
+
+
 def compare_dataframes(df1, agg_func, df2, epsilon):
     """Check consistency of values across month and year dataframes.
-    
+
     Args:
-        df1: Dataframe with monthly values
-        agg_func: Dataframe with annual values
-        df2: Dataframe with quarterly values (unused)
+        df1: Dataframe to aggregate
+        agg_func: Aggregation function
+        df2: Dataframe to check against
         epsilon: Tolerance for numeric precision
-        
+
     Returns:
         Boolean value indicating whether all values are within epsilon.
     """
-    if agg_func.__name__ == 'aggregate_levels':
-        columns = get_levels(df1.columns)
-        df1 = build_levels_df(df1)
-    return np.all(abs((agg_func(df1[columns]) - df2[columns]).dropna()) < epsilon)
+    return np.all(abs((agg_func(df1) - build(df2)).dropna()) < epsilon)
+
+
+###############################################################################
+
+
+def month_to_year(dfm):
+    df_levels = aggregate_levels(dfm, ANNUAL_FREQUENCY)
+    df_rates = aggregate_rates(dfm, ANNUAL_FREQUENCY)
+    return df_levels.join(df_rates)
+
+
+def quarter_to_year(dfq):
+    df_levels = aggregate_levels(dfq, ANNUAL_FREQUENCY)
+    df_rates = aggregate_rates(dfq, ANNUAL_FREQUENCY)
+    return df_levels.join(df_rates)
+
+
+def runner(feed):
+    return np.all(compare_dataframes(*test_case) for test_case in feed)
