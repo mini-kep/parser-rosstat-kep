b141b15547047b0f89b6c9f9fe522af7d7b3c9b6
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-09-02 09:37:21 +0300

    #91: preferred test for open_csv() with temp file

--- src/csv2df/tests/test_reader.py
+++ src/csv2df/tests/test_reader.py
@@ -40,6 +40,28 @@ class MockPath2(type(Path())):
     def open(self, *arg, **kwarg):
         m = mock_open()
         return m(*arg, **kwarg)
+    
+@pytest.fixture    
+def temp_path():
+    with NamedTemporaryFile() as f:
+        abspath = f.name
+    p = Path(abspath)    
+    p.write_text("abc\n123")
+    return p
+
+class Test_open_csv:    
+
+    def test_on_string_argument_raises_TypeError(self):
+        path_string = 'abc.csv'
+        with pytest.raises(TypeError):
+            open_csv(path_string)
+
+    def test_on_Path_runs_with_no_error(self, temp_path):
+        assert open_csv(temp_path)
+
+    def test_on_Path_provides_readable_input(self, temp_path):
+        with open_csv(temp_path) as f:            
+            assert f.readlines() == ['abc\n', '123']    
    
     
 # TODO: implement tests
