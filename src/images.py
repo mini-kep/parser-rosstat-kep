0afb47d27f75f93e62bb234cd73cd0182ce58ff8
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-09-04 13:25:35 +0300

    refactored classes at images.py for #78

--- src/images.py
+++ src/images.py
@@ -5,31 +5,15 @@ import matplotlib.pyplot as plt
 
 from config import find_repo_root
 
-import getter
+def get_pix_folder(subfolder):    
+    return find_repo_root() / 'output' / 'png' / subfolder
 
 
-#Псеводкод использования:
-# выбрать конкретный показатель 
-# ts = dfm.RETAIL_SALES_FOOD_bln_rub
-# создать инстанс графика
-# spline1 = Spline(ts)
-# нарисовать граифк в консоли
-# spline1.show()
-# сохранить в дефолтное для этого типа графоков место в файлоой системе
-# spline1.save()
-# сохранить в названный каталог (для тестирвоания)
-# spline1.save(folder)
+start = datetime.date(1998, 12, 31)
+end = datetime.date(2017, 12, 31)
+DEFAULT_TIMERANGE = start, end
 
-
-# Этот код должен быть в __main__ для отладки для
-# Spline
-# IndicatorChart
-
-DEFAULT_TIMERANGE = datetime.date(1998, 12, 31), datetime.date(2017, 12, 31)
-
-DEFAULT_GPARAMS = {'timerange':DEFAULT_TIMERANGE, 
-                        'figsize':(10, 10), 'style':'ggplot', 'facecolor':'white',
-                        'auto_x':False, 'axis_on':True}
+# graphics parameters in dicts
 
 SPLINE_GPARAMS = {'timerange':DEFAULT_TIMERANGE, 
                         'figsize':(2, 0.6), 'style':'ggplot', 'facecolor':'white',
@@ -41,50 +25,47 @@ INDICATOR_GPARAMS = {'timerange':DEFAULT_TIMERANGE,
 
 
 class GraphBase:
-    #TODO: need real short docstring what this class is.
-    def __init__(self, tss, params=DEFAULT_GPARAMS, name='untitled'):
+    """Parent class for standard project charts.     
+    
+       Parent to:
+            Spline
+            IndicatorChart    
+    """ 
+    def __init__(self, ts, params):
         """
         Args:
-            tss: list of timeseries to plot.
-                for a simple chart len(tss) = 1
+            ts (pd.TimeSeries): 
             params: chart formatting/style parameters      
         """
-        self.tss = tss
+        self.ts = ts
         self.params = params
         self.fig = None
+        self.subfolder = None       
+
+    def _get_filename(self):
+        # FIXME will need to put frequency in, not todo now
+        return f'{self.ts.name}.png'
         
-        self.rootfolder = find_repo_root()
-        self.pngfolder = self.rootfolder / 'output' / 'png'
-        self.subfolder = 'misc'     # will be overridden by child classes
-        self.name = name
-
-    def __del__(self):
-        # plt.close()
-        pass
-
+    def _get_path(self):        
+        return str(get_pix_folder(self.subfolder) / self._get_filename(self))
+            
     def plot(self):
-        """
-        What does this function do?
-        Is there special kind of formatting applied to all graphs?
-        
-        Returns:
-            None          
-        """
+        #FIXME: желательно разделить на подгтовку осей / отрисовку / выдачу фигуры
         plt.style.use(self.params['style'])
         fig = plt.figure(figsize=self.params['figsize'])
         axes = fig.add_subplot(1, 1, 1, facecolor=self.params['facecolor'])
-        for ts in self.tss:
-            axes.plot(ts)
+        # собственно plot, его желталеьно выделить чтобы перезагрузить в BranchChart
+        axes.plot(ts)
+        # ----
         axes.set_xlim(self.params['timerange'])
         if self.params['auto_x']:
             fig.autofmt_xdate()
         if not self.params['axis_on']:
-            plt.axis('off')
-        
+            plt.axis('off')        
         self.fig = fig
 
     def save(self):
-        if self.fig == None:
+        if self.fig is None:
             raise ValueError('Figure is empty, call GraphBase.plot() first')
         else:
             self.fig.savefig(self.get_file_path())
@@ -94,30 +75,27 @@ class GraphBase:
         del self.fig
         self.fig = None
 
-    def get_file_path(self):
-        file_path = str(self.pngfolder / self.subfolder / (self.name+'.png'))
-        return file_path
-
 
 class Spline(GraphBase):
-    def __init__(self, tss, name):
-        assert len(tss) == 1
-        super().__init__(tss, params=SPLINE_GPARAMS, name=name)
+    def __init__(self, ts, name=None):
+        super().__init__(ts, params=SPLINE_GPARAMS)
         self.subfolder = 'splines'
 
 
 class IndicatorChart(GraphBase):
-    def __init__(self, tss, name):
-        assert len(tss) > 0
-        super().__init__(tss, params=INDICATOR_GPARAMS, name=name)
-        if len(tss) > 1:
-            self.subfolder = 'mul_indicators'
-        else:
-            self.subfolder = 'indicators'        
+    def __init__(self, df, name=None):
+        super().__init__(df, params=INDICATOR_GPARAMS)
+        self.subfolder = 'indicators'        
 
 
+class BranchChart(GraphBase):
+    def __init__(self, df, name=None):
+        super().__init__(df, params=INDICATOR_GPARAMS)
+        self.subfolder = 'indicators'   
+        
+    # TODO: overload drawing method    
 
-
+    # TODO: overload naming method  
 
 def plot_all_images(save=True):
     dfa, dfq, dfm = [getter.get_dataframe(freq) for freq in 'aqm']
@@ -128,30 +106,54 @@ def plot_all_images(save=True):
 
     graphs = []
 
-# type 1 and 2 graphs
+    # type 1 and 2 graphs
     for col in cols:
         ts = df[col]
         graphs.append(Spline        ([ts], col))
         graphs.append(IndicatorChart([ts], col))
 
-# type 3 graphs
-    inds = ['RETAIL_SALES_FOOD_bln_rub', 
-            'RETAIL_SALES_NONFOOD_bln_rub']
-    tss = [df[col] for col in inds]
-    name = '_AND_'.join(sorted(inds))
-    graphs.append(IndicatorChart(tss, name))
-    
-# plotting and saving all
+   
+    # plotting and saving all
     for graph in graphs:
         graph.plot()
         graph.save()
         graph.close()
 
 
+# Checklist
 
-    
-# TODO: EP - add tasks.py command/integrate to finaliser.py for latest graphs.   
-    
+# SK:
+# ключевое:    
+# - [ ] разделили GraphBase.plot() 
+# дополниельно:    
+# - [ ] сделали класс BranchChart
+# - [ ] assert/тесты для публичных классов
+
+# EP:
+# - [ ] ввели новый нейминг для картинок с разной частотой типа mCPI_rog.png
+# - [ ] создаем очередь что рисовать 
+# - [ ] запускаем очередь что рисовать 
+# - [ ] tasks.py command/integrate to finaliser.py for latest graphs 
+
+
+# Checklist 2
+# - [ ] убрали name из инита + добавили set_name()  
+
 if __name__ == "__main__":
-    plot_all_images()    
     
+    import getter
+    dfa, dfq, dfm = (getter.get_dataframe(freq) for freq in 'aqm')
+    
+    ts = dfm.CPI_rog
+    s = Spline(ts, 'name')
+    ic = IndicatorChart(ts, 'name2')
+    
+    s.plot()
+    ic.plot()
+    
+    
+    # type 3 graphs
+    varnames = ['RETAIL_SALES_FOOD_bln_rub', 
+                'RETAIL_SALES_NONFOOD_bln_rub']
+    df = dfm[varnames]
+    bc = BranchChart(df)
