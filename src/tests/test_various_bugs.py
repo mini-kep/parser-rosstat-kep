1b3379518f52c8a1f425ed05101ce4490763ec1f
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-12-07 04:22:06 +0300

    Update test_various_bugs.py

--- src/tests/test_various_bugs.py
+++ src/tests/test_various_bugs.py
@@ -5,43 +5,36 @@ from pathlib import Path
 import pandas as pd
 from io import StringIO
 
-# HS: import the find_repo_root
-from config import find_repo_root
-#from config import PathHelper, find_repo_root
-# HS
+from config import find_repo_root, InterimCSV
 from download.download import make_url
 
-ROOT = find_repo_root()
-
-
 def test_time_index_is_included_in_access():
+    
+    # EP: there shoudl be  areason why we need this?
 
     def make_path(freq):
-        folder = ROOT / 'data' / 'processed' / 'latest'
+        folder = find_repo_root() / 'data' / 'processed' / 'latest'
         return str(folder / "df{}.csv".format(freq))
 
     for freq in 'aqm':
-        # a workaround for Windows problem
-        # https://github.com/pandas-dev/pandas/issues/15086
+        # a workaround for Windows problem https://github.com/pandas-dev/pandas/issues/15086
         content = Path(make_path(freq)).read_text()
         filelike = StringIO(content)
         df = pd.read_csv(filelike)
         assert df.columns[0] == 'time_index'
 
-# HS: Remove this, since uses the PathHelper module. Ask if it should be replaced in some way
-#def test_csv_has_no_null_byte():
-#    csv_path = PathHelper.locate_csv(2015, 2)
-#    z = csv_path.read_text(encoding='utf-8')
-#    assert "\0" not in z
+def test_csv_has_no_null_byte():
+    csv_path = InterimCSV(2015, 2).path
+    z = csv_path.read_text(encoding='utf-8')
+    assert "\0" not in z
 
 # HS: add a test function for the make_url -- check that the formatting works
+# EP: 'will_not_work_without_string_format' - not very conclusive what is means
 def test_make_url_will_not_work_without_string_format():
-
     year = 2017
     month = 2
     url = make_url(year, month)
     assert '{' not in url
-
 
 if __name__ == "__main__":
     pytest.main([__file__])
