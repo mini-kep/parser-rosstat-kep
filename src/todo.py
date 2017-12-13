885bed875b9795e14897dcc0ffebc6bd1a39ceb9
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-12-13 15:59:15 +0300

    Update and rename todo.md to todo.py

---
+++ src/todo.py
@@ -0,0 +1,25 @@
+Issues
+=======
+- add more parsing definitions
+- add variable name descriptions
+
+Todo
+====
+- make Excel + add new sheet 
+- revisions case
+- finish variable grouping
+- copy_latest needs fix
+- concat INDPRO from two series
+- use df_check.py
+- add vintage.py integration test
+
+Not todo
+========
+- CPI_yoy ?
+- renaming variables after parsing?
+- smplify validator?
+
+Discuss
+=======
+- may use a webhook to upload to database
+- add uploader code
