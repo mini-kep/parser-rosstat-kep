18026ce5d65b22ff9c71accfa1ae5277b0419ce0
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2018-01-31 18:01:33 +0300

    before deleting emitter.py

---
+++ src/kep/csv2df/util_to_float.py
@@ -0,0 +1,31 @@
+import re
+COMMENT_CATCHER = re.compile("\D*(\d+[.,]?\d*)\s*(?=\d\))")
+
+def to_float(text: str, i=0):
+    """Convert *text* to float() type.
+
+    Returns:
+        Float value, or False if not successful.
+    """
+    i += 1
+    if i > 5:
+        raise ValueError("Max recursion depth exceeded on '{}'".format(text))
+    if not text:
+        return False
+    text = text.replace(",", ".")
+    try:
+        return float(text)
+    except ValueError:
+        # note: order of checks important
+        if " " in text.strip():  # get first value '542,0 5881)'
+            return to_float(text.strip().split(" ")[0], i)
+        if ")" in text:  # catch '542,01)'
+            match_result = COMMENT_CATCHER.match(text)
+            if match_result:
+                text = match_result.group(0)
+                return to_float(text, i)
+        if text.endswith(".") or text.endswith(","):  # catch 97.1,
+            return to_float(text[:-1], i)
+        return False
+
+# TODO: move test from emitter.py 
