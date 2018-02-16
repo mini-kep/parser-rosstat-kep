24047b3deec1d1975c2d35bd9076b47875ca45d7
Dmitry Shlagoff <wessmoke@gmail.com>
2018-02-16 18:56:38 +0700

    Added 'good path' test

--- src/kep/validation/tests/test_validation.py
+++ src/kep/validation/tests/test_validation.py
@@ -1,7 +1,7 @@
 import pytest
 
 from kep.vintage import Vintage
-from kep.validation.checkpoints import validate2, Checkpoint
+from kep.validation.checkpoints import validate2, Checkpoint, ValidationError
 
 
 @pytest.fixture(scope="module")
@@ -9,27 +9,42 @@ def dataframe():
     return Vintage(2017, 12).dfs["a"]
 
 
-# TODO: this is 'bad path' checkpoints, need a fixture + test with 'good path' checkpoints
 @pytest.fixture(scope="module")
-def checkpoints():
+def bad_path_checkpoints():
     return [
         Checkpoint(
             date="1999",
             freq="z",
             name="some_really_long_test_variable",
-            value="100.0"
+            value=100.0,
         )
     ]
 
 
-def test_validate_raises_value_error_on_missed_checkpoint_in_dataframe(dataframe, checkpoints):
-    with pytest.raises(ValueError):
-        validate2(dataframe, checkpoints, [])
+@pytest.fixture(scope="module")
+def good_path_checkpoints():
+    return [
+        Checkpoint(
+            date="1999",
+            freq="a",
+            name="AGROPROD_yoy",
+            value=103.8,
+        )
+    ]
 
 
-def test_validate_raises_value_error_on_uncovered_checkpoint_in_dataframe(dataframe):
-    with pytest.raises(ValueError):
-        validate2(dataframe, [], [])
+def test_validate_raises_value_error_on_missed_checkpoint_in_dataframe(dataframe, bad_path_checkpoints):
+    with pytest.raises(ValidationError):
+        validate2(dataframe, bad_path_checkpoints, [])
+
+
+def test_validate_good_path(dataframe, good_path_checkpoints):
+    validate2(dataframe, good_path_checkpoints, [])
+
+
+def test_validate_raises_value_error_on_uncovered_checkpoint_in_dataframe(dataframe, good_path_checkpoints):
+    with pytest.raises(ValidationError):
+        validate2(dataframe, good_path_checkpoints, good_path_checkpoints, strict=True)
 
  # TODO: need simplest tests touching  
  # def find_missed_checkpoints(df, checkpoints):
