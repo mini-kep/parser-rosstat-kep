eafd0f2fdc6f5c978ddbc1fa4b5bf3b870ad717c
Dmitry Shlagoff <wessmoke@gmail.com>
2018-02-16 19:10:50 +0700

    Added some non-exhaustive tests for validation funcs

--- src/kep/validation/tests/test_validation.py
+++ src/kep/validation/tests/test_validation.py
@@ -1,7 +1,13 @@
 import pytest
 
 from kep.vintage import Vintage
-from kep.validation.checkpoints import validate2, Checkpoint, ValidationError
+from kep.validation.checkpoints import (
+    validate2,
+    Checkpoint,
+    ValidationError,
+    find_missed_checkpoints,
+    find_uncovered_column_names
+)
 
 
 @pytest.fixture(scope="module")
@@ -10,7 +16,7 @@ def dataframe():
 
 
 @pytest.fixture(scope="module")
-def bad_path_checkpoints():
+def upfront_absent_checkpoints():
     return [
         Checkpoint(
             date="1999",
@@ -33,12 +39,12 @@ def good_path_checkpoints():
     ]
 
 
-def test_validate_raises_value_error_on_missed_checkpoint_in_dataframe(dataframe, bad_path_checkpoints):
+def test_validate_raises_value_error_on_missed_checkpoint_in_dataframe(dataframe, upfront_absent_checkpoints):
     with pytest.raises(ValidationError):
-        validate2(dataframe, bad_path_checkpoints, [])
+        validate2(dataframe, upfront_absent_checkpoints, [])
 
 
-def test_validate_good_path(dataframe, good_path_checkpoints):
+def test_validate_returns_without_error_on_good_path(dataframe, good_path_checkpoints):
     validate2(dataframe, good_path_checkpoints, [])
 
 
@@ -46,6 +52,17 @@ def test_validate_raises_value_error_on_uncovered_checkpoint_in_dataframe(datafr
     with pytest.raises(ValidationError):
         validate2(dataframe, good_path_checkpoints, good_path_checkpoints, strict=True)
 
- # TODO: need simplest tests touching  
- # def find_missed_checkpoints(df, checkpoints):
- # def find_uncovered_column_names(df, checkpoints):        
+
+def test_find_missed_checkpoints_returns_missed_checkpoints(dataframe, upfront_absent_checkpoints):
+    assert find_missed_checkpoints(dataframe, upfront_absent_checkpoints) == set(upfront_absent_checkpoints)
+
+
+def test_find_missed_checkpoints_returns_empty_set_on_valid_data(dataframe, good_path_checkpoints):
+    assert find_missed_checkpoints(dataframe, good_path_checkpoints) == set()
+
+
+def test_find_uncovered_column_names_returns_valid_data(dataframe, upfront_absent_checkpoints, good_path_checkpoints):
+    assert len(find_uncovered_column_names(dataframe, [])) == len(dataframe.columns)
+    assert len(find_uncovered_column_names(dataframe, upfront_absent_checkpoints)) == len(dataframe.columns)
+    assert len(find_uncovered_column_names(dataframe, good_path_checkpoints)) == \
+        len(dataframe.columns) - len(good_path_checkpoints)
