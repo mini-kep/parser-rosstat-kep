89e52fb84c087a961a1a2343ff45b5281670d739
epogrebnyak <e.pogrebnyak@gmail.com>
2018-07-03 08:46:20 +0300

    minimal example truncated

--- src/python-minimal/reader.py
+++ src/python-minimal/reader.py
@@ -1,5 +1,8 @@
 # Parse data from CSV files
 
+# This example is work in progress to create minimal example
+# Full code code is at src/python/manage.py 
+# Point of entry of parsing algorithm:  src.python.dispatch.evaluate() 
 
 # PSEUDOCODE
 # ---------- 
@@ -11,12 +14,15 @@
 # 4. combine data from tables into three dataframes based on frequency (dfa, dfq, dfm)
 # 5. write dfa, dfq, dfm as csv files to disk 
 
-# MINIMAL EXAMPLE
-# ---------------
+# READ CSV AND SPLIT INTO TABLES (steps 1 and 2 of pseudocode)
+# ------------------------------------------------------------
+
+import csv
+import re
+from enum import Enum, unique
 
 def read_csv(file): 
     """Read csv file as list of lists / matrix"""
-    import csv
     fmt=dict(delimiter='\t', lineterminator='\n')
     table = []
     with open(file, 'r', encoding='utf-8') as f:
@@ -25,60 +31,66 @@ def read_csv(file):
     return table
 
 
-def is_year(x: str):
-    """Simplified filter for numeric value in first column"""
-    return x.startswith('199') or x.startswith('20')  
+@unique
+class State(Enum):
+    INIT = 0
+    DATA = 1
+    HEADERS = 2
+
+YEAR_CATCHER = re.compile("\D*(\d{4}).*")
+
+
+def get_year(string: str, rx=YEAR_CATCHER):
+    """Extracts year from *string* using *rx* regex.
+
+       Returns:
+           Year as integer
+           False if year is not valid or not in plausible range."""
+    match = re.match(rx, string)
+    if match:
+        year = int(match.group(1))
+        if year >= 1991 and year <= 2050:
+            return year
+    return False
+
+def is_year(string: str) -> bool:
+    return get_year(string) is not False
+
+
+def split_to_tables(rows):
+    """Yield Tab() instances from *rows* list of lists."""
+    datarows = []
+    headers = []
+    state = State.INIT
+    for row in rows:
+        # is data row?
+        if is_year(row[0]):
+            datarows.append(row)
+            state = State.DATA
+        else:
+            if state == State.DATA:
+                # table ended, emit it
+                yield Table(headers, datarows)
+                headers = []
+                datarows = []
+            headers.append(row)
+            state = State.HEADERS
+    # still have some data left
+    if len(headers) > 0 and len(datarows) > 0:
+        yield Table(headers, datarows)
+
+  
+class Table:
+    def __init__(self, headers, datarows):
+        self.headers = headers
+        self.datarows = datarows
 
     
 if __name__ == "__main__":
     # read csv    
-    table = read_csv("tab.csv")
-   
-    def extract_table_segment(table, start_line_contain, table_end_marker):
-        table_segment = []
-        we_are_in_segment = False
-        i = 0
-        while i <= len(table):
-            row = table[i]
-            if start_line_contains in row[0]:
-                we_are_in_segment = True
-            if we_are_in_segment:
-                table_segment.append(row)
-                del table[i]
-            else:    
-                i += 1                
-            if row[0].strip().startswith(table_end_marker):
-                break
-
-        return table_segment   
-
-    start_line_contains = "Объем ВВП"
-    table_end_marker = "2018"  
-    segment1 = extract_table_segment(table, start_line_contains, table_end_marker)
-    # current checkpoint - we were able to cut segment1 a segment from table
-    #                     based on start_line_contains and table_end_marker
-    
-    
-    #slice_data, slice_header = extract(table, start_line_contains, table_endwith)
-    
-    
-    # next:
-    # name_mapper is a dictionary that maps "Объем ВВП" to "GDP"
-    # unit_mapper is a dictionary that maps "млрд.рублей" to  "bln_rub"
-    
-    
-    # name = get_name(name_mapper, slice_header) 
-    # unit = get_unit(unit_mapper, slice_header)
-    # label is name_unit
-    _label = "GDP_bln_rub"
-    
-    # get_datapoints(data=slice_data, row_pattern="YQQQQ", label=_label)
-    # sort to streams of annual, quarterly and monthly data 
-    
-    
-    # ENHANCEMENTS
-    # ------------
-    # - write test for minimal example
-    # - same without table_endswith - stop table before when next text is encountered
-    # - trailing tables like "Индекс промышленного производства" + "в % к предыдущему периоду"
-    # - cleaning values like "20171)"
+    csv_rows = read_csv("tab.csv")
+    # split csv rows to tables
+    # NOTES: 
+    #   - split_to_tables() is a state machine to traverse through csv file 
+    #   - in julia the data structure for Table class is likely to be different     
+    tables = split_to_tables(csv_rows)
