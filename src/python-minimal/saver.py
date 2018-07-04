8d0625ec4d4cbc08c534fb3f4910591956340d2f
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2018-07-04 20:19:27 +0300

    todo: number cleaners

--- src/python-minimal/saver.py
+++ src/python-minimal/saver.py
@@ -3,3 +3,31 @@
 #  - (optional) checkpoints
 #  - saving to dataframes
 #  - checking dataframes
+
+
+from reader import to_values
+from parsing_definition import NAMERS, UNITS
+
+values = to_values('tab.csv', UNITS, NAMERS)
+
+
+messed_years = set()
+messed_values = set()
+for v in values:
+    year = v['year']
+    value = v['value'].replace(',', '.')
+    try:
+        int(year)
+    except:    
+        messed_years.add(year)
+    try: 
+        float(value)
+    except:         
+        messed_values.add(value)
+print(list(messed_years))
+print(list(messed_values))
+
+#TODO: clean 1 or 2 occurences of '\d)'
+  
+    
+    
