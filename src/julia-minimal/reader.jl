c3683b23d2bfc6035ac0fa9893b12f84a4fc189e
sergey <uristmcgollor@gmail.com>
2018-07-04 14:34:24 +0300

    Finished csv loading

--- src/julia-minimal/reader.jl
+++ src/julia-minimal/reader.jl
@@ -89,21 +89,108 @@ function assign_units(channel_in, channel_out, unit_mapper)
     end
 end
 
+function datarows_length(table)
+    rows_width = [maximum([i for i in 1:size(table.datarows[j], 1) if table.datarows[j][i] != ""]) for j in 1:size(table.datarows, 1)]
+    return maximum(rows_width)
+end
+
+function get_row_format(table, key=nothing)
+    _key = key == nothing ? datarows_length(table) - 1 : key
+    return Dict(
+        1 + 4 + 12 => join(["Y", "A", repeat("Q", 4), repeat("M", 12)]),
+        1 + 4 => join(["Y", "A", repeat("Q", 4)]),
+        1 + 12 => join(["Y", "A", repeat("M", 12)]),
+        12 => join(["Y", repeat("M", 12)]),
+        4 => join(["Y", "QQQQ"]),
+        "fiscal" => join(["Y", "A", repeat("M", 11)])
+        )[_key]
+end
+
+function assert_unit_found(table, unit)
+    if table.unit != unit
+        throw(DomainError())
+    end
+end 
+
+function emit_datapoints_from_row(row, channel, label, row_format)
+    occurences = ""
+    year = nothing
+    for i in 1:length(row_format)
+        value = row[i]
+        letter = row_format[i]
+        occurences = join([occurences, letter])
+        if letter == 'Y'
+            year = value
+        end
+        if value != "" && contains("AQM", string(letter))
+            put!(channel, Dict(
+                "label" => label,
+                "freq" => lowercase(letter),
+                "year" => year,
+                "period" => length(matchall(Regex(string(letter)), occurences)),
+                "value" => value
+            ))
+        end
+    end
+end
+
+function emit_datapoints_from_table(table, channel, label, row_format)
+    for i in 1:size(table.datarows, 1)
+        emit_datapoints_from_row(table.datarows[i], channel, label, row_format)
+    end
+end
+
+function make_label(name, unit)
+    return join([name, "_", unit])
+end
+
+function get_datapoints(channel_in, channel_out, parsing_definition)
+    while true
+       table = take!(channel_in)
+       assert_unit_found(table, parsing_definition["unit"])
+       row_format = get_row_format(table, get(parsing_definition, "reader", nothing))
+       label = make_label(parsing_definition["name"], table.unit)
+       emit_datapoints_from_table(table, channel_out, label, row_format)
+    end
+end
+
+function to_values(filename, channel, unit_mapper_list, parsing_definitions)
+    function unit_mapper(table)
+        table.unit = extract_unit(table, unit_mapper_list)
+        return table
+    end
+    table = readdlm("tab.csv", '\t')
+    channel_1 = Channel(8)
+    channel_2 = Channel(8)
+    @schedule split_to_tables(table, channel_1)
+    @schedule assign_units(channel_1, channel_2, unit_mapper_list)
+    @schedule get_datapoints(channel_2, channel, parsing_definitions[1])
+end
+
 unit_mapper = [
     ["млрд.рублей", "bln_rub"], 
     ["период с начала отчетного года в % к соответствующему периоду предыдущего года", "ytd"],
     ["в % к соответствующему периоду предыдущего года", "yoy"],
     ["в % к предыдущему периоду", "rog"],
     ["отчетный месяц в % к предыдущему месяцу", "rog"]]
+parsing_definition1 = Dict("headers" => ["Объем ВВП"],
+                           "name" => "GDP",
+                           "unit" => "bln_rub")
+parsing_definition2 = Dict("headers" => ["Консолидированные бюджеты субъектов"],
+                           "name" => "VARX",
+                           "unit" => "bln_rub",
+                           "reader" => "fiscal")
 
 table = readdlm("tab.csv", '\t')
 heads = table[1:35,1]
-print(heads)
 channel_1 = Channel(8) # the max amount of stored objects
 channel_2 = Channel(8)
+channel_3 = Channel(8)
 @schedule split_to_tables(table, channel_1)
 @schedule assign_units(channel_1, channel_2, unit_mapper)
-single_table = take!(channel_2)
-print(single_table)
+@schedule get_datapoints(channel_2, channel_3, parsing_definition2)
+datapoint = take!(channel_3)
+print(datapoint)
+
 end
 
